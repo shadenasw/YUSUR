3afaf011ffe463be3110d23479a66f052b824159
Noura fadil <110945017+Noura-sun@users.noreply.github.com>
2024-12-11 16:33:01 +0300

    Initial Commit

---
+++ YUSUR/YUSURApp.swift
@@ -0,0 +1,17 @@
+//
+//  YUSURApp.swift
+//  YUSUR
+//
+//  Created by Noura  on 10/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct YUSURApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
